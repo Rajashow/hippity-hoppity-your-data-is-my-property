e3933348a17e369b9285b2ca2c14aedbf1320628
Rajashow <rajashowparajuli@gmail.com>
2020-05-14 22:28:20 -0400

    code block?

---
+++ toggle_codeblocks.py
@@ -0,0 +1,5 @@
+from IPython.display import HTML
+
+
+def code_button():
+    return HTML('''<script>code_show=true;function code_toggle() {  if (code_show){  $('div.input').hide();  } else {  $('div.input').show();  }  code_show = !code_show } </script> <form align="center" action="javascript:code_toggle()"><input type="submit" class = "btn btn-primary btn-block" value="Click here to toggle on/off the raw code."></form>''')
